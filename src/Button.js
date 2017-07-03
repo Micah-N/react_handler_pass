d43c4f2fa3aebe28049714c748c6936bd3a3a90e
Micah-N <mnetz@2ndwatch.com>
2017-07-03 10:04:09 -0700

    Added files

---
+++ src/Button.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+export class Button extends React.Component {
+  render() {
+    return (
+      <button onClick={this.props.talk}>
+        Click me!
+      </button>
+    );
+  }
+}
